d161736a8ec275d212c3203ab9de85a9e3d10b5e
Allison Verhulst <122564024+verhulsta532@users.noreply.github.com>
2023-10-25 16:05:38 -0500

    Added grid class to the purchase and gallery

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 //Hamburger menu function
-function hamburger(){
-  var menu = document.getElementById("menu-links");
-  var logo = document.getElementById("tte-logo");
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("tte-logo");
 
-  if(menu.style.display === "block" && logo.style.display === "none"){
-    menu.style.display = "none";
-    logo.style.display = "block";
-  } else{
-    menu.style.display = "block";
-    logo.style.display = "none";
-  }
+
+    if (menu.style.display === "block" && logo.style.display === "none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    } else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
 }
 
 let slideIndex = 1;
@@ -17,26 +18,30 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
