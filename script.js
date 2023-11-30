1ddc61b85b4f4b91010f602f5a0b1352c1f7ef2f
Allison Verhulst <122564024+verhulsta532@users.noreply.github.com>
2023-11-30 15:10:31 -0600

    Added dots to the slideshow

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -38,5 +39,10 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
+
